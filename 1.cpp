52d817b85b1988e0b2785636d23ebf4490f4f133
NehaJOShi259 <nehajoshi6350@gmail.com>
2025-05-06 13:52:04 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,61 +2,50 @@
 using namespace std;
 
 struct Student {
-    int rollno;
-    int marks;
-    int chain;
+    int rollno = -1;
+    int marks = -1;
 };
 
-int hashFunction(int key) {
-    return key % 10;
+int hashFunction(int rollno) {
+    return rollno % 10;
 }
 
 void insert(Student table[], int rollno, int marks) {
     int index = hashFunction(rollno);
-    if (table[index].rollno == -1) {
-        table[index].rollno = rollno;
-        table[index].marks = marks;
-        table[index].chain = -1;
-        return;
-    }
 
-    int start = index;
     while (table[index].rollno != -1) {
         index = (index + 1) % 10;
-        if (index == start) return;
     }
 
     table[index].rollno = rollno;
     table[index].marks = marks;
-    table[index].chain = -1;
 }
 
 void display(Student table[]) {
-    cout << "Index\tRollNo\tMarks\tChain\n";
+    cout << "Index\tRollNo\tMarks\n";
     for (int i = 0; i < 10; i++) {
         if (table[i].rollno != -1)
-            cout << i << "\t" << table[i].rollno << "\t" << table[i].marks << "\t" << table[i].chain << "\n";
+            cout << i << "\t" << table[i].rollno << "\t" << table[i].marks << "\n";
         else
-            cout << i << "\t---\t---\t---\n";
+            cout << i << "\t---\t---\n";
     }
 }
 
 int main() {
     Student table[10];
-    for (int i = 0; i < 10; i++) {
-        table[i].rollno = -1;
-        table[i].marks = -1;
-        table[i].chain = -1;
-    }
 
-    int rollnos[] = {31, 13, 14, 51, 16, 71, 48, 19};
-    int marks[] = {85, 78, 90, 72, 88, 65, 91, 69};
-
-    for (int i = 0; i < 8; i++) {
-        insert(table, rollnos[i], marks[i]);
-    }
+    insert(table, 31, 85);
+    insert(table, 13, 78);
+    insert(table, 14, 90);
+    insert(table, 51, 72);
+    insert(table, 16, 88);
+    insert(table, 71, 65);
+    insert(table, 48, 91);
+    insert(table, 19, 69);
 
     display(table);
+
     return 0;
 }
+
 
