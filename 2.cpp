1fef16b0695abe44ee5fd5532840e46d98865b36
NehaJOShi259 <nehajoshi6350@gmail.com>
2025-05-06 17:16:05 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,46 +1,29 @@
-#include <iostream>
-#include <list>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-const int TABLE_SIZE = 26;
-
-int hashFunction(string word) {
+int hashFunction(const string &word) {
     return word[0] - 'a';
 }
 
-void insert(vector<list<string>> &table, string word) {
-    int index = hashFunction(word);
-    table[index].push_back(word);
-}
-
-bool search(vector<list<string>> &table, string word) {
-    int index = hashFunction(word);
-    for (auto &w : table[index])
-        if (w == word)
-            return true;
-    return false;
-}
-
 int main() {
-    vector<list<string>> table(TABLE_SIZE);
+    vector<list<string>> table(26);
     vector<string> words = {
-        "apple", "banana", "grape", "orange", "peach", "plum", "berry", "melon", "kiwi", "lemon",
-        "mango", "guava", "apricot", "pear", "cherry", "fig", "date", "lime", "papaya", "tamarind",
-        "coconut", "nectarine", "blueberry"
+        "apple", "banana", "grape", "orange", "peach", "plum", "berry", "melon",
+        "kiwi", "lemon", "mango", "guava", "apricot", "pear", "cherry", "fig",
+        "date", "lime", "papaya", "tamarind", "coconut", "nectarine", "blueberry"
     };
 
-    for (string w : words) insert(table, w);
+    for (const string &w : words)
+        table[hashFunction(w)].push_back(w);
 
-    string input;
-    cout << "Enter words to check spelling (type 'exit' to stop):\n";
-    while (true) {
-        cin >> input;
-        if (input == "exit") break;
-        if (search(table, input))
-            cout << "Correct spelling.\n";
-        else
-            cout << "Incorrect spelling.\n";
+    string word;
+    cout << "Enter word (type 'exit' to stop):\n";
+    while (cin >> word && word != "exit") {
+        bool found = false;
+        for (const string &w : table[hashFunction(word)])
+            if (w == word) found = true;
+        cout << (found ? "Correct\n" : "Incorrect\n");
     }
     return 0;
 }
+
